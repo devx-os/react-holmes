e4f70597943802270003ac5d7580157231e024ee
riktar <tartaglia.riccardo@gmail.com>
2022-05-23 15:05:17 +0200

    start

---
+++ src/hooks/index.js
@@ -0,0 +1,13 @@
+import useHolmesState from "./useHolmesState/userHolmesState";
+import useHolmesSelector from "./useHolmesSelector/useHolmesSelector";
+import useHolmesValue from "./useHolmesValue/useHolmesValue";
+import setHolmesReducer from "./setHolmesReducer/setHolmesReducer";
+import useHolmesReducer from "./useHolmesReducer/useHolmesReducer";
+
+export {
+    useHolmesState,
+    useHolmesSelector,
+    useHolmesValue,
+    setHolmesReducer,
+    useHolmesReducer
+}
