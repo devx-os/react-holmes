a10006649e56c0da48d4893aa340de808cbfe133
riktar <tartaglia.riccardo@gmail.com>
2022-05-23 15:58:46 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-let $global = null;
-$global =
-  typeof global !== 'undefined'
-    ? global
-    : typeof window !== 'undefined'
-      ? window
-      : {};
+require('holmes')
+const hooks = require('./hooks/index')
 
-if (!$global) throw new Error('Global object not found.');
-
-$global.__holmesObservableMap = new Map();
-
-export function getGlobalContext() {
-  return $global.__holmesObservableMap;
-}
-
-export function getObservable(key) {
-  if (!$global.__holmesObservableMap.has(key)) {
-    return null;
-  }
-  return $global.__holmesObservableMap.get(key);
-}
+export default {...hooks}
