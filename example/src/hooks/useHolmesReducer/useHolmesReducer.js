ab02455a363ffbc8262c880d6f3aca94830e4245
AldoNoschese96 <aldo.noschese@webeetle.it>
2022-05-26 15:07:19 +0200

    Fix

--- example/src/hooks/useHolmesReducer/useHolmesReducer.js
+++ example/src/hooks/useHolmesReducer/useHolmesReducer.js
@@ -1,15 +1,16 @@
-import {useEffect, useRef} from 'react';
+import { useEffect, useRef } from "react";
 
-import {getGlobalContext} from '../../holmes';
+import { getGlobalContext } from "../../holmes";
 import useHolmesValue from "../useHolmesValue/useHolmesValue";
 
-const useHolmesReducer = (key = '') => {
-  const dispatch = useRef(() => {})
+const useHolmesReducer = (key = "") => {
+  const dispatch = useRef(() => {});
 
   useEffect(() => {
     const context = getGlobalContext();
-    const dispatchGlobal = context.has(`${key}_dispatch`) ? context.get(`${key}_dispatch`) : null;
-    //if(!dispatchGlobal) throw new Error(`${key} has not been setted, set a reducer first with setHolmesReducer() hook`);
+    const dispatchGlobal = context.has(`${key}_dispatch`)
+      ? context.get(`${key}_dispatch`)
+      : null;
     dispatch.current = dispatchGlobal;
   }, []);
 
